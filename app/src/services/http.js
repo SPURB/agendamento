c3359edb90253a0f363a9ed16d1c372e56e6a34f
m-dantas <mauriciodantas.contato01@bol.com.br>
2020-03-19 17:24:12 -0300

    chore(app): add chamadas na api e validação de formulário

--- app/src/services/http.js
+++ app/src/services/http.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
-
+console.log(process.env)
+const baseURLD = 'http://spurbsp163:7080/agendamento/api.php/' // baseURL em development
+const baseURLP = 'https://servicos.spurbanismo.sp.gov.br/agendamento/api/' // baseURL em production
 export const http = axios.create({
-  baseURL: 'http://spurbsp163:7080/agendamento/api.php/',
+  baseURL: process.env.NODE_ENV === 'development' ? baseURLD : baseURLP,
   headers: {
     Current: 'default'
   }
