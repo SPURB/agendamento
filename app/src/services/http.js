a7b5addb3673cc0fd2f9b015d81c4ae0ad7f365c
yubathom <yubathom@gmail.com>
2020-03-20 15:03:22 -0300

    feat: inclui autorização em posts

--- app/src/services/http.js
+++ app/src/services/http.js
@@ -1,10 +1,15 @@
 import axios from 'axios'
-console.log(process.env)
+import fechadura from '@spurb/fechadura'
+
 const baseURLD = 'http://spurbsp163:7080/agendamento/api.php/' // baseURL em development
 const baseURLP = 'https://servicos.spurbanismo.sp.gov.br/agendamento/api/' // baseURL em production
+
+const baseURL = () => process.env.NODE_ENV === 'production' ? baseURLP : baseURLD
+const Current = () => fechadura(JSON.parse(process.env.VUE_APP_API_TOKEN), 'bicho').encript
+
 export const http = axios.create({
-  baseURL: process.env.NODE_ENV === 'development' ? baseURLD : baseURLP,
+  baseURL: baseURL(),
   headers: {
-    Current: 'default'
+    Current: Current()
   }
 })
